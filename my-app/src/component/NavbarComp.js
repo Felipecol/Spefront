194af434f7ad24e32caa2cfcea1ba13d93728e75
Andres Felipe GIRON CARVAJAL <af.giron-carvajal@groupeonepoint.com>
2021-10-02 04:47:03 +0200

    windostest

--- my-app/src/component/NavbarComp.js
+++ my-app/src/component/NavbarComp.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
 import {
     BrowserRouter as Router,
     Switch,
