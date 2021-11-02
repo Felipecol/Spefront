69e0346a7ac009ffa7c1513c2a7d352ac1c75032
Felipe Giron <5928944-FelpGrn@users.noreply.gitlab.com>
2021-11-02 20:54:30 +0100

    formNetlify24

--- my-app/src/component/MailComp.js
+++ my-app/src/component/MailComp.js
@@ -7,7 +7,7 @@ export default function MailComp(){
         <div className="container">
             <div className="container2">
                 <h1>Contactez-nous</h1>
-                <form name="contact" method="post" >
+                <form name="contact" method="post" action="/Yeptest" >
                     <input type="hidden" name="form-name" value="contact" />
                     <p>
                         <label htmlFor="name" class="card-title">Name</label> <br />
