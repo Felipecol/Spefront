8daf1e4b829307cd65a03d4e2094ad25829293ff
Felipe Giron <5928944-FelpGrn@users.noreply.gitlab.com>
2021-11-02 21:40:43 +0100

    formNetlify30

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import FooterComp from "./component/FooterComp";
 
 import React from "react";
-import {Nav, Navbar} from "react-bootstrap";
-import {Animated} from "react-animated-css";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-import PlancheComp from "./component/PlancheComp";
-import MailComp from "./component/MailComp";
-import RouesComp from "./component/RouesComp";
-import SkateComp from "./component/SkateComp";
-import TrucksComp from "./component/TrucksComp";
-import Yeptest from "./component/Yeptest";
-import HomeComp from "./component/HomeComp";
-import pagenotfound from "./component/pagenotfound";
 import NavbarComp from "./component/NavbarComp";
 
 
